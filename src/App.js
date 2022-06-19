1df05a7f95e901b719831821ff9550da45ef4046
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-19 17:00:06 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './MyComponents/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
 
 function App() {
+  const onDelete = () => {
+    console.log("I am onDelete");
+  };
+
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get this job done1",
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get this job done2",
+    },
+    {
+      sno: 3,
+      title: "Go to the office",
+      desc: "You need to go to the market to get this job done3",
+    },
+  ];
   return (
     <>
-    <Header/>
+      <Header title="My Todos List" />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
     </>
   );
 }
