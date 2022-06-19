87d36dff107bb0df02b280d86f325eed18ac46ce
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-19 16:59:18 +0530

    adding Header to our application

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
