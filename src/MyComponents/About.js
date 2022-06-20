cc8a0d49ddf7e15cf15a6dfd461b959e9584a3df
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-20 16:19:45 +0530

    Adding About.js to project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+    <div>This is about page</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit ratione, nisi optio est vel qui, veniam corporis itaque soluta aperiam voluptate dicta. Dolore minima dolor adipisci a aliquam esse ipsam saepe quod inventore.</p>
+    </>
+  )
+}
