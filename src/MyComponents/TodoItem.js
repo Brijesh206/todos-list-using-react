fc3df8ef34fa418bb90c79934f6fe4517f6cf227
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-20 16:20:44 +0530

    Todo Items updated

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 export const TodoItem = ({ todo, onDelete }) => {
   return (
     <div>
-      <h4>{todo.title}</h4>
+      <h5>{todo.title}</h5>
       <p>{todo.desc}</p>
       <button
         className="btn btn-sm btn-danger"
@@ -13,6 +13,7 @@ export const TodoItem = ({ todo, onDelete }) => {
       >
         Delete
       </button>
+      <hr/>
     </div>
   );
 };
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { AddTodo } from "./AddTodo";
 import { TodoItem } from "./TodoItem";
 // import App from '../App';
 
 export const Todos = (props) => {
+  let myStyle = {
+    minHeight : '70vh',
+    margin : "40px auto"
+  }
   return (
-    <div className="container">
-      <h3 className="text-center my-3">Todos List</h3>
+    <div className="container" style={myStyle}>
+      <h4 className=" my-3">Todos List</h4>
       {props.todos.length === 0
         ? "No todos to display"
         : props.todos.map((todo) => {
