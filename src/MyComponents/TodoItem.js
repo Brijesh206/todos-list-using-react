1f781a4193121da600a781b0dac037902f996805
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-20 07:52:08 +0530

    updating TodoItem

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -7,7 +7,9 @@ export const TodoItem = ({ todo, onDelete }) => {
       <p>{todo.desc}</p>
       <button
         className="btn btn-sm btn-danger"
-        onDelete={onDelete}
+        onClick={()=>{
+          onDelete(todo);
+        }}
       >
         Delete
       </button>
