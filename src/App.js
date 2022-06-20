d264160fa470bf6e860bdebe78623247df1a2fac
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-20 07:52:42 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { Footer } from "./MyComponents/Footer";
+import { useState } from "react";
 
 function App() {
-  const onDelete = () => {
-    console.log("I am onDelete");
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo", todo);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
   };
 
-  let todos = [
+  const [todos, setTodos] = useState([
     {
       sno: 1,
       title: "Go to the market",
@@ -25,7 +30,7 @@ function App() {
       title: "Go to the office",
       desc: "You need to go to the market to get this job done3",
     },
-  ];
+  ]);
   return (
     <>
       <Header title="My Todos List" />
