df29fcbbc0d3d94930254467f9e06f80ad1b9a83
Brijesh <brijeshbhesaniya12@gmail.com>
2022-06-24 11:39:04 +0530

    Updating About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export const About = () => {
+  let myStyle = {
+    minHeight : '71.3vh',
+    margin : "40px auto"
+  }
   return (
     <>
-    <div>This is about page</div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur velit ratione, nisi optio est vel qui, veniam corporis itaque soluta aperiam voluptate dicta. Dolore minima dolor adipisci a aliquam esse ipsam saepe quod inventore.</p>
+    <div style={myStyle}><h3>This is about page</h3>
+    <p>Hello users!, I am Brijesh and curruntly this todolist application is in devlopment stage. So please stay tuned. You will get an update soon.</p>
+    </div>
     </>
   )
 }
